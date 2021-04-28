1460fd82643b8bb858d0ae1840d077171ba58c9a
Dawid Binkowski <d.k.binkowski@gmail.com>
2021-04-28 16:41:30 +0200

    add some text to Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,7 +12,31 @@ function Work() {
 			<div className="work_example">
 				<ul className="work_example_elements">
 					<li>
-						Expedia Meeting Market<span>View more</span>
+						Expedia Meeting Market{" "}
+						<a href="https://www.expedia.com/">View more</a>
+					</li>
+
+					<li>
+						Melanie Daveid{" "}
+						<a href="https://melaniedaveid.com/">View more</a>
+					</li>
+					<li>
+						Austria Summer Moments{" "}
+						<a href="https://www.austria.info/en">View more</a>
+					</li>
+					<li>
+						Row Low{" "}
+						<a href="http://www.rowlow.github.io/">View more</a>
+					</li>
+					<li>
+						Digging in the Crates{" "}
+						<a href="https://ditcent.myshopify.com/">View more</a>
+					</li>
+					<li>
+						Infinity Car Configurator{" "}
+						<a href="https://infiniti-carconfigurator.com/">
+							View more
+						</a>
 					</li>
 				</ul>
 			</div>
